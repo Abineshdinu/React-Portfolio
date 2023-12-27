27129b382d70b1c4146eb6225996a74568117db9
Abineshdinu <abineshdinu@gmail.com>
2023-12-27 17:54:31 +0530

    First commit

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,77 +1,69 @@
-import React, { Fragment } from 'react'
-import { SiCodechef, SiHackerrank } from 'react-icons/si';
+import React, { Fragment } from "react";
 
 function Resume() {
   return (
     <Fragment>
-        <div className="col p-4"> 
-            <div className="p-4 bg-dark slide-in-top">
-                
-                <div className="row">
-                    <div className="col-6">
-                        <h2 className="ff-jose fw-bold ls-2">YAGNIK GOHIL</h2>
-                    </div>
-                </div>
-                <hr></hr>
-                <div className="row">
-                    <div className="col-md-6">
-                        <h4 className="ff-jose ls-2">EXPERIENCE</h4>
-                        <ul>
-                            <li>
-                                <h4 className="ff-jose my-1 text-red">Trainee Software Engineer</h4>
-                                <h6 className="blue-label px-2 py-1">Jul 2021 - Apr 2022</h6>
-                                <p className="m-0">TatvaSoft · Full-time</p>
-                                <p>Ahmedabad, Gujarat, India</p>
-                            </li>
-                            <li>
-                                <h4 className="ff-jose my-1 text-red">Internship Trainee</h4>
-                                <h6 className="blue-label px-2 py-1">Dec 2020 - Apr 2021</h6>
-                                <p className="m-0">TatvaSoft · Internship</p>
-                                <p>Ahmedabad, Gujarat, India</p>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="col-md-6">
-                        
-                        <h4 className="ff-jose ls-2">EDUCATION</h4>
-                        <ul>
-                            <li>
-                                <h4 className="ff-jose my-1 text-red">BE - Computer Engineering</h4>
-                                <h6 className="blue-label px-2 py-1">2017 - 2021</h6>
-                                <p className="m-0">Gujarat Technological University</p>
-                                <p>CGPA: 8.05</p>
-                            </li>
-                            <li>
-                                <h4 className="ff-jose my-1 text-red">Higher Secondary School</h4>
-                                <h6 className="blue-label px-2 py-1">2017</h6>
-                                <p className="m-0">Shree B.M.Commerce High School</p>
-                                <p>Grade: 71.69 %</p>
-                            </li>
-                        </ul>
-
-                    </div>
-                </div>
-
-                <hr></hr>
-                
-                <div className="row">
-                    <div className="col-md-6">
-                        <h4 className="ff-jose ls-2">PROFILE</h4>
-                        <h1>
-                            <a href="https://www.codechef.com/users/yagnik_003" target="/blank" className="text-white text-decoration-none mx-2">
-                                <SiCodechef className="zoom-on-hover"/>
-                            </a> 
-                            <a href="https://www.hackerrank.com/Yagnik_003" target="/blank" className="text-white text-decoration-none mx-2">
-                                <SiHackerrank className="zoom-on-hover"/>
-                            </a> 
-                        </h1>
-                    </div>
-                </div>
-                
+      <div className="col p-4">
+        <div className="p-4 bg-dark slide-in-top">
+          <div className="row">
+            <div className="col-6">
+              <h2 className="ff-jose fw-bold ls-2">Abineshwaran</h2>
             </div>
+          </div>
+          <hr></hr> 
+          <div className="row">
+            <div className="col-md-6">
+              <h4 className="ff-jose ls-2">EXPERIENCE</h4>
+              <ul>
+                <li>
+                  <h4 className="ff-jose my-1 text-red">Internship</h4>
+                  <h6 className="blue-label px-2 py-1">
+                    March 2023 - jun 2023
+                  </h6>
+                  <p className="m-0">
+                    XYLOINC. IT Services and IT Consulting, Coimbatore
+                  </p>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-6">
+              <h4 className="ff-jose ls-2">EDUCATION</h4>
+              <ul>
+                <li>
+                  <h4 className="ff-jose my-1 text-red">
+                    BSC - Computer Technology
+                  </h4>
+                  <h6 className="blue-label px-2 py-1">2021 - 2024</h6>
+                  <p className="m-0">Dr.N.G.P Arts and Science College, Coimbatore</p>
+                  <p>Grade: 68 %</p>
+                </li>
+                <li>
+                  <h4 className="ff-jose my-1 text-red">
+                    Higher Secondary School
+                  </h4>
+                  <h6 className="blue-label px-2 py-1">2019 - 2021</h6>
+                  <p className="m-0">
+                    St. Joseph's Higher Secondary School, Ooty{" "}
+                  </p>
+                  <p>Grade: 77 %</p>
+                </li>
+                <li>
+                  <h4 className="ff-jose my-1 text-red">Secondary School</h4>
+                  <h6 className="blue-label px-2 py-1">2018 - 2019</h6>
+                  <p className="m-0">
+                    St. Joseph's Higher Secondary School, Ooty{" "}
+                  </p>
+                  <p>Grade: 61 %</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <hr></hr>
         </div>
+      </div>
     </Fragment>
-  )
+  );
 }
 
-export default Resume
+export default Resume;
